3011309a0c01c5c32c723b4e8a6821ce477e280a
ant1freeze <alexandr.krpv@gmail.com>
2020-05-01 19:37:03 +0300

    Update generate_hosts.py

--- day2/generate_hosts.py
+++ day2/generate_hosts.py
@@ -6,7 +6,7 @@ print(os.listdir(path))
 if 'hosts.yaml' in os.listdir(path):
     os.remove(os.path.join(path,file))
 for i in range(1,254):
-    dev = 'sw' + str(i) + ':\n    hostname: 10.130.6.' + str(i) + '\n    groups:\n        - switch\n'
+    dev = 'sw' + str(i) + ':\n    hostname: 192.168.1.' + str(i) + '\n    groups:\n        - switch\n'
     print(dev)
 
     with open(os.path.join(path, file), 'a') as yaml:
